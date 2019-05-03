4f731abf3305baca822239b898d9ffc3eb4e1d67
bmzhu2 <brian.m.zhu@gmail.com>
2019-05-03 01:05:20 -0700

    added 404 page

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -5,6 +5,7 @@ import Modal from './session/modal';
 import NavigationBar from './navigation_bar';
 import Splash from './splash/splash';
 import Discover from './discover/discover';
+import ErrorPage from './error_page';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
       <Switch>
         <AuthRoute exact path="/" component={Splash} />
         <Route path="/discover" component={Discover} />
+        <Route path="/" component={ErrorPage}/>
       </Switch>
 
       {/* <MusicPlayer/> */}
---
+++ frontend/components/error_page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = (props) => {
+  return (
+    <div class="main-body">
+      <div class="error-404">404 This page does not exist</div>
+    </div>
+  )
+}
+
+export default ErrorPage;
