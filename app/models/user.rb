8e38a6d737357f7f319ee910a6c0fce6b0f0e249
bmzhu2 <brian.m.zhu@gmail.com>
2019-05-03 00:07:58 -0700

    attempted to add AWS, not sure

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,8 @@ class User < ApplicationRecord
 
   after_initialize :ensure_session_token
 
+  has_one_attached :profile_picture
+
   def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
 
