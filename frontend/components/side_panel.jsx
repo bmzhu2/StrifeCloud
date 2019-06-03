af6674aab798603b50f5ef305dc244f8c9c8d01c
bmzhu2 <brian.m.zhu@gmail.com>
2019-06-03 11:14:34 -0700

    Adds linkedin and portfolio links

--- frontend/components/side_panel.jsx
+++ frontend/components/side_panel.jsx
@@ -5,7 +5,11 @@ class SidePanel extends React.Component {
   render() {
     return(
       <div className="side-panel">
-        <a href="https://github.com/bmzhu2/StrifeCloud" className="github-link"></a>
+        <section className="links">
+          <a href="https://github.com/bmzhu2/StrifeCloud" className="icon fab fa-github"></a>
+          <a href="https://www.linkedin.com/in/brianmzhu/" className="icon fab fa-linkedin"></a>
+          <a href="http://www.brianmzhu.com" className="icon fas fa-user"></a>
+        </section>
       </div>
     )
   }
