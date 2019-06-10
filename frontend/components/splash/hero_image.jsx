3b5d2107ab15d31619e4715bee0fd4b821c18f2d
bmzhu2 <brian.m.zhu@gmail.com>
2019-06-10 00:33:29 -0700

    Adds search function to splash page and fixes search query not showing up in nav bar's search

--- frontend/components/splash/hero_image.jsx
+++ frontend/components/splash/hero_image.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {openModal} from '../../actions/modal_actions';
+import {withRouter} from 'react-router-dom'
 
-const HeroImage = ({openModal}) => {
+const HeroImage = (props) => {
   return(
     <section className="hero-image">
       <div className="hero-text">
         <h1>What's next in music is first on StrifeCloud</h1>
         <p>Upload your first track and begin your journey. StrifeCloud gives you space to create, find your fans, and connect with
         other artists.</p>
-        <button onClick={() => openModal('signup')}>Start uploading today</button>
+        <button onClick={() => props.history.push("/upload/")}>Start uploading today</button>
       </div>
     </section>
   )
 }
 
-const mapDispatchToProps = dispatch => ({
-  openModal: mode => dispatch(openModal(mode))
-})
-
-export default connect(null, mapDispatchToProps)(HeroImage);
+export default withRouter(HeroImage);
